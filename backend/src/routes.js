e09be8cc0d2e9beb0043ba47f8159e05a539413f
jhonatan2104 <xdjhonatan0@gmail.com>
2019-11-12 23:38:35 -0300

    Model/Controller/upload

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
+const sessionController = require("./controllers/SessionController");
+const spotController = require("./controllers/SpotController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post("/users", (req, res) => {
-    return res.json(req.body);
-});
+routes.post("/sessions", sessionController.store);
+routes.get("/spots", spotController.index);
+routes.post("/spots", upload.single("thumbnail"),spotController.store);
 
 module.exports = routes;
